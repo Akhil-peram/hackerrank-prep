b31b49bdeeb7a0eb91511027fbb3ac246b81181c
Akhil <96526208+Akhil-peram@users.noreply.github.com>
2026-02-14 19:55:16 +0530

    Update panagram.py

--- panagram.py
+++ panagram.py
@@ -1,1 +1,14 @@
+def isPanagram(s:str):
+  return 'panagram' if set('abcdefghijklmnopqrstuvwxyz').issubset(s.lower()) else "Not panagram"
 
+sentence = "The quick brown fox jumps over the lazy dog "
+
+print(isPanagram(sentence))
+
+"""
+
+a string line or a sentence is panagram if all the alphabets exist in the string 
+
+otherwise not a panagram 
+
+"""
